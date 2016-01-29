c76870acfa6bd704598e21a0dea5a8bbfdf37bb7
yvonna <yvonnap.contreras@gmail.com>
2016-01-29 14:45:28 -0800

    Fix HTML indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,26 @@
 function Pizza(size, toppings, quantity) {
-  this.size = size;
+  this.sizePizza = sizePizza;
   this.toppings = toppings;
   this.quantity = parseInt(quantity);
   this.cost = 0;
 }
 
-Pizza.prototype.sizeCost = function(){
-  var sizeCost;
-  if (this.size === small){
-  sizeCost = 5;
-  } if(this.size === medium){
-  sizeCost = 8;
-  } if(this.size === large){
-   sizeCost = 10;
-  }
+Pizza.prototype.sizePrice = function(){
+  var sizePrice;
+    if (this.sizePizza === "Small"){
+    sizePrice = 5;
+  } else if(this.sizePizza === "Medium"){
+    sizePrice = 8;
+  } else if(this.sizePizza === "Large"){
+    sizePrice = 10;
+  } return (sizePrice);
 }
- 
+
+Pizza.prototype.about = function(){
+  var toppingsChoice = this.toppings.toString();
+  return this.quantity + " " + this.sizePizza + "Pizza's with" + toppingsChoice;
+}
+
 Pizza.prototype.numberOf = function(){
   var totalNumber = 0;
   this.pizzasOrdered.forEach(function(Pizza){
@@ -30,13 +35,13 @@ Pizza.prototype.totalCost = function(){
     orderTotal = orderTotal + Pizza.quantity;
   })
 }
-
+//-----------------------------jQuery-------------------------------------//
 $(document).ready(function() {
   $("form#dallas").submit(function(event){
     event.preventDefault();
-    var selectedSize =
+    var selectedSize = $(this).find("input[name='optionsRadiosSML']:checked").val();
+    var selectedToppings = $(this).find("input[name='toppingCheck']:checked").val();
 
-  });
-
-
+console.log(newPizza);
+});
 });
