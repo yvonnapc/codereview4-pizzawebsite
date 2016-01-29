0e0374f32160ecc5bef64dad47ff458a20112bf6
yvonna <yvonnap.contreras@gmail.com>
2016-01-29 08:59:59 -0800

    Add pizza prototype

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,14 @@ function Pizza(size, toppings) {
   this.cost = 0;
 }
 
+Pizza.prototype.cost = function(){
+  if(this.size === small){
+  } return (this.cost = 5);
+  if(this.size === medium){
+  } return (this.cost = 8);
+  if (this.size === large){
+  } return(this.cost = 10);
+}
 
 // $(document).ready(function() {
 //
